bf41579aa09ca8b8b4116ed8248842981e51f2fc
Enrique Vázquez <quique_vzquez@yahoo.es>
2023-11-20 20:22:34 +0100

    First draft of App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return (
+      <div>
+        <h1>Computer Vision</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Insertar URL or escribir prompt:
+            <input type="text" value={this.state.url} onChange={this.handleChange} />
+          </label>
+          <br />
+          <button type="submit">Analizar imagen</button>
+          <button type="submit">Generar imagen</button>
+        </form>
+      </div>
+    );
 }
 
 export default App;
